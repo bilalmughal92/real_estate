8e893d95a44d41cf5f927614d17c767ae4d160b0
Bilal <bill4pak@gmail.com>
2017-07-07 21:19:06 +0500

    Initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+
+  def login
+    
+  end
+
+  def signup
+    
+  end
+
+end
---
+++ test/controllers/users_registeration_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UsersRegisterationControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
