eb69011d3542d9c6a92e0c890dd2d4461cf95f2d
Bilal <bill4pak@gmail.com>
2017-07-08 14:56:57 +0500

    glyphicons, login functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :check_current_user
+
+  def current_user
+    AdminUserDetail.find_by(email: session[:current_user_email])
+  end
+
+  def check_current_user
+    # byebug
+    if current_user.present?
+    else
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,17 @@
 class UsersController < ApplicationController
 
   def login
-    byebug
+    user = AdminUserDetail.where(email: user_params[:email], password: user_params[:password])
+    if user.present?
+      session[:current_user_email] = user.first.email
+    else
+      flash[:error] = "Email or password is not correct."
+    end
+    redirect_to '/'
   end
 
   def signup
-    if AdminUserDetail.create(signup_params)
+    if AdminUserDetail.create(user_params)
       flash[:success] = "Successfully Register."
     else
       flash[:error] = "Cannot Signup Admin User."
@@ -15,7 +21,7 @@ class UsersController < ApplicationController
 
   private
 
-  def signup_params
+  def user_params
     params.require(:admin_user_detail).permit!
   end
 
