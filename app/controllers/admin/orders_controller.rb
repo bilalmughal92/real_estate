3b1be2be4f7c9bfeda64b5fbf6dfb2c13bb8da01
Bilal Naeem <bill4pak@gmail.com>
2017-10-20 17:31:38 +0000

    etc

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,4 +4,10 @@ class Admin::OrdersController < Admin::BaseController
   def index
     @orders = Order.all
   end
+
+  def destroy
+    @order = Order.find(params[:id])
+    @order.destroy
+    redirect_to admin_orders_path
+  end
 end
